4667daaa98d3d9829c10cd3ff67517d23821b070
Pedro Filho <pedro@filho.me>
2020-05-21 21:15:33 +0100

    Fixing boilerplate

---
+++ webpack/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = "../src/";
+
+module.exports = {
+  entry: {
+    background: path.join(__dirname, srcDir + "background.ts"),
+    options: path.join(__dirname, srcDir + "options.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "public/", to: ".", globOptions: { ignore: [".DS_Store"] } },
+      ],
+    }),
+  ],
+};
