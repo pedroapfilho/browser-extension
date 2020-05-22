b6d4e03864ff2b29dda3816a2532bf38de7692f7
Pedro Filho <pedro@filho.me>
2020-05-22 17:14:02 +0100

    Adding optimizations for webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,11 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const { optimize } = require("webpack");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new optimize.AggressiveMergingPlugin(),
+    new optimize.OccurrenceOrderPlugin(),
+  ],
 });
